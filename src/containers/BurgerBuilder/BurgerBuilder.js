0e1aaaa0b8e43370863da553ade4fb9103b3dcc3
Lester <lester.deguzman@tup.edu.ph>
2020-03-28 15:55:57 +0800

    Change on BurgerBuilder states

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,7 @@ class BurgerBuilder extends Component {
     state = {
         ingredients: null,
         totalPrice: 4,
-        purchasable: false,
+        purchasable: true,
         purchasing: false,
         loading: false,
         error: false
