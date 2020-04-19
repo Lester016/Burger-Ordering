8a84fdb3e74f543868897ea442269e7bc6e5bf15
Lester <lester.deguzman@tup.edu.ph>
2020-04-19 13:06:01 +0800

    Add Lazy Loading using  React.Lazy() in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Authentication from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
+import Spinner from "./components/UI/Spinner/Spinner";
+
+const Checkout = React.lazy(() => import("./containers/Checkout/Checkout"));
+const Orders = React.lazy(() => import("./containers/Orders/Orders"));
+const Authentication = React.lazy(() => import("./containers/Auth/Auth"));
 
 class App extends Component {
   componentDidMount() {
@@ -19,14 +21,16 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" exact component={Authentication} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-            <Redirect to="/" />
-          </Switch>
+          <Suspense fallback={<Spinner />}>
+            <Switch>
+              <Route path="/checkout" component={Checkout} />
+              <Route path="/orders" component={Orders} />
+              <Route path="/auth" exact component={Authentication} />
+              <Route path="/logout" component={Logout} />
+              <Route path="/" exact component={BurgerBuilder} />
+              <Redirect to="/" />
+            </Switch>
+          </Suspense>
         </Layout>
       </div>
     );
