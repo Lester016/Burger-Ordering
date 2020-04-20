5981a165331ec423c4c9ff3e8d4c66ccfabe6352
Lester <lester.deguzman@tup.edu.ph>
2020-04-20 17:01:44 +0800

    Add Tests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import axios from "../../axios-orders";
 import * as actions from "../../store/actions/index";
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   // constructor(props) {
   //     super(props);
   //     this.state = {...}
